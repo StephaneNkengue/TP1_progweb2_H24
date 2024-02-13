b3392034492c6e1c28d90a819e8745ff7153d106
RENÉ-MARCELLIN TCHOKOMI <2136882@CegepGranby.qc.ca>
2024-02-13 13:00:11 -0500

    Réglages de conflits

--- obj/Release/net8.0/TP1.AssemblyInfo.cs
+++ obj/Release/net8.0/TP1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25535b08f3a0840cf4999ac2659eca19c2dd142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aaa1ad9d0b8c67d67a9c8bbd096191762a53cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
