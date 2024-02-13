977407ca14fc98353ee1a2d7133d7091e6d8fa24
StephaneNkengue <2210962@cegepgrnby.qc.ca>
2024-02-13 12:25:59 -0500

    Rajout du thème bootstrap

--- obj/Debug/net8.0/TP1.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25535b08f3a0840cf4999ac2659eca19c2dd142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6fb7d180939a0b0519d535521679b5a61950c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
